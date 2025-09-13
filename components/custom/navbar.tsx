dd226768b05e4cc61ef746d5bd9b4dfb55b09809
Justin Miguel Reyes <virozzegt@gmail.com>
2025-09-14 00:43:39 +0800

    feat: (#9) fetch accounts from API

--- components/custom/navbar.tsx
+++ components/custom/navbar.tsx
@@ -1,91 +1,140 @@
 "use client";
-import { useState } from 'react';
-import { usePathname } from 'next/navigation';
-import { selectItems } from '@/lib/data';
+import { useEffect, useState } from 'react';
+import { usePathname, useRouter } from 'next/navigation';
+import { Account } from '@/types/accounts.types';
 import Link from 'next/link';
 import Image from 'next/image';
+import { fetchAccounts } from '@/store/accounts.store';
 import { Button } from '@/components/ui/button';
-
 import {
-  Select,
-  SelectContent,
-  SelectGroup,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
+	Select,
+	SelectContent,
+	SelectGroup,
+	SelectItem,
+	SelectTrigger,
+	SelectValue,
 } from "@/components/ui/select";
-
 import {
-  TypographyH3,
+	TypographyH3,
 } from "@/components/custom/typography";
-
-import { 
-  PlusIcon 
+import {
+	Edit,
+	Loader2,
+	PlusIcon
 } from 'lucide-react';
+import { 
+	ContextMenu,
+	ContextMenuTrigger 
+} from '../ui/context-menu';
+import { ContextMenuContent, ContextMenuItem } from '@radix-ui/react-context-menu';
 
 function Navbar() {
-  const [open, setOpen] = useState(false);
-  const pathname = usePathname();
-  const disableSelect = pathname !== '/pages/transactions';
+	const router = useRouter();
+	const [isLoading, setIsLoading] = useState(true);
+	const [open, setOpen] = useState(false);
+	const pathname = usePathname();
+	const disableSelect = pathname !== '/pages/transactions';
+	const [accounts, setAccounts] = useState<Account[]>([]);
 
-  return (
-    <nav className='p-2 
-      w-full 
-      flex 
-      justify-between
-      items-center 
-      bg-white 
-      border-b-2 border-black'
-    >
-      <Link href={'/pages/transactions'} 
-        className='flex space-x-2 items-center'
-      >
-        <Image
-          src='/icons/favicon.ico'
-          alt='Gaston Icon'
-          height={32}
-          width={32}
-        />
-        <TypographyH3>
-          Gastos
-        </TypographyH3>
-      </Link>
+	const handleNewAccount = () => {
+		setOpen(false)
+		router.push('/pages/accounts/create')
+	};
 
-      {/* Select Accounts Dropdown */}
-      <Select open={open} onOpenChange={setOpen} disabled={disableSelect}>
-        <SelectTrigger 
-          className="w-[180px] 
-          bg-primary 
-          border-2 border-black 
-          max-w-[120px]
-          text-sm"
-        >
-          <SelectValue placeholder="Accounts" />
-        </SelectTrigger>
+	const handleEdit = (uuid: string) => {
+		setOpen(false);
+		router.push(`/pages/accounts/${uuid}`);
+	}
 
-        <SelectContent className='border-2 border-black'>
-          <SelectGroup>
-            {selectItems.map((item, index) => (
-              <SelectItem 
-                value={item.value} 
-                key={index}
-              >
-                {item.title}
-              </SelectItem>
-            ))}
-            <Link href={'/pages/accounts/create'}>
-              <Button 
-                onClick={() => setOpen(false)}  
-                className='mt-2 w-full bg-gray-300 text-black'
-              >
-                <PlusIcon className='-mr-1'/> New Account
-              </Button>
-            </Link>
-          </SelectGroup>
-        </SelectContent>
-      </Select>
-    </nav>
-  )
+	useEffect(() => {
+		fetchAccounts()
+			.then((accounts) => {
+				setAccounts(accounts);
+				setIsLoading(false);
+			})
+			.catch((error) => {
+				if (error instanceof Error) {
+					console.error(error.message)
+					setIsLoading(false);
+				};
+			})
+	}, [open])
+
+	return (
+		<nav className='p-2 
+			w-full 
+			flex 
+			justify-between
+			items-center 
+			bg-white 
+			border-b-2 border-black'
+		>
+			<Link href={'/pages/transactions'}
+				className='flex space-x-2 items-center'
+			>
+				<Image
+					src='/icons/favicon.ico'
+					alt='Gaston Icon'
+					height={32}
+					width={32}
+				/>
+				<TypographyH3>
+					Gastos
+				</TypographyH3>
+			</Link>
+
+			{/* Select Accounts Dropdown */}
+			<Select open={open} onOpenChange={setOpen} disabled={disableSelect}>
+				<SelectTrigger
+					className="w-[180px] 
+					bg-primary 
+					border-2 border-black 
+					max-w-[120px]
+					text-sm"
+				>
+					<SelectValue placeholder="Accounts" />
+				</SelectTrigger>
+
+				<SelectContent className='border-2 border-black'>
+					<SelectGroup>
+						{isLoading ?
+							<div className='flex flex-col justify-center'> 
+								<Loader2 className='w-full h-6 w-6 mt-1 mb-1 text-gray-600 animate-spin'/>
+							</div>
+						:
+							<>
+								{accounts.map((account, index) => (
+								<ContextMenu key={index}>
+									<ContextMenuTrigger>
+										<SelectItem value={account.uuid}>
+											{account.name}
+										</SelectItem>
+									</ContextMenuTrigger>
+									<ContextMenuContent className='bg-primary rounded-md'>
+										<ContextMenuItem 
+											className="flex items-center px-2 py-1 text-sm cursor-pointer hover:bg-gray-100 rounded-sm"
+												onClick={() => handleEdit(account.uuid)}
+											>
+											<Edit className="mr-2 h-4 w-4" />
+											Edit
+										</ContextMenuItem>
+									</ContextMenuContent>
+								</ContextMenu>
+								))}
+							</>
+						}
+						<Button
+							onClick={handleNewAccount}
+							className='w-full bg-gray-300 text-black'
+							disabled={isLoading}
+						>
+							<PlusIcon className='-mr-1' /> New Account
+						</Button>
+					</SelectGroup>
+				</SelectContent>
+			</Select>
+		</nav>
+	)
 };
 
 export default Navbar;
