f34bf0469f1e5ab8c6b04c6d39776f5e266f4779
Justin Miguel Reyes <virozzegt@gmail.com>
2025-09-13 18:00:45 +0800

    fix: improper utils usage

--- utils/session.ts
+++ utils/session.ts
@@ -1,15 +1,6 @@
 import { authClient } from "@/lib/auth-client";
-import { auth } from "@/lib/auth";
-import { headers } from 'next/headers';
 
 export async function fetchSession() {
     const { data: session, error } = await authClient.getSession();
     return { session, error};
-};
-
-export async function fetchUserID() {
-  const session = await auth.api.getSession({
-    headers: await headers()
-  });
-  return session?.user.id;
 };
