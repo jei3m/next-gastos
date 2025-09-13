5469da0ab446164883a49a65923c38c3a1c4e6bf
Justin Miguel Reyes <virozzegt@gmail.com>
2025-09-14 00:44:06 +0800

    feat: (#9) CRUD store functions for accounts

---
+++ store/accounts.store.ts
@@ -0,0 +1,63 @@
+import { CreateAccount, EditAccount } from "@/types/accounts.types";
+
+export const fetchAccounts = async () => {
+    try {
+        const res = await fetch('http://localhost:3000/api/accounts');
+        const data = await res.json();
+        return(data.data)
+    } catch (error) {
+        if (error instanceof Error) {
+            throw Error(error.message)
+        };
+        throw Error('Failed to Fetch Accounts');
+    }
+};
+
+export const fetchAccountByID = async (uuid:string) => {
+    try {
+        const res = await fetch(`http://localhost:3000/api/accounts/${uuid}`);
+        const data = await res.json();
+        return(data.data)
+    } catch (error) {
+        if (error instanceof Error) {
+            throw Error(error.message)
+        };
+        throw Error('Failed to Fetch Account');
+    }
+};
+
+export const createAccount = async (account: CreateAccount) => {
+    try {
+        const res = await fetch('http://localhost:3000/api/accounts',{
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify(account)
+        });
+        const data = await res.json();
+        if (!res.ok) throw Error(data.message);
+        return data.data  ;
+    } catch (error) {
+        if (error instanceof Error) {
+            throw Error(error.message)
+        };
+        throw Error('Failed to Create Account');
+    };
+};
+
+export const editAccount = async (uuid: string, account: EditAccount) => {
+    try {
+        const res = await fetch(`http://localhost:3000/api/accounts/${uuid}`,{
+            method: 'PUT',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify(account)
+        });
+        const data = await res.json();
+        if (!res.ok) throw Error(data.message);
+        return data.data;
+    } catch (error) {
+        if (error instanceof Error) {
+            throw Error(error.message);
+        };
+        throw Error('Failed to Edit Account')
+    }
+};
