4f30d93ee32817e387120c1abeba5dcdb83ea708
Justin Miguel Reyes <virozzegt@gmail.com>
2025-09-28 02:13:24 +0800

    feat: (#10) create and get routes

---
+++ app/api/categories/route.ts
@@ -0,0 +1,72 @@
+import { NextRequest } from "next/server";
+import crypto from 'crypto';
+import { db } from "@/utils/db";
+import { success, fail } from "@/utils/helpers";
+import { responseRow } from "@/types/response.types";
+import { createCategory, getCategories } from "@/sql/categories/categories.sql";
+import { fetchUserID } from "@/lib/auth-session";
+
+export async function POST(req: NextRequest) {
+    try {
+        const {
+            name,
+            type
+        } = await req.json();
+
+        const [resultCreate] = await db.query<responseRow[]>(
+            createCategory(),
+            {
+                actionType: 'create',
+                uuid: crypto.randomUUID(),
+                userID: await fetchUserID(),
+                name,
+                type
+            }
+        );
+
+        const parsedData = JSON.parse(
+            resultCreate[1][0].response
+        );
+
+        if (parsedData.responseCode !== 200) {
+            return fail(
+                parsedData.responseCode,
+                parsedData.responseMessage
+            )
+        };
+
+        return success({
+            data: parsedData
+        });
+        
+    } catch (error) {
+        return fail(
+            500,
+            error instanceof Error
+                ? error.message
+                : 'Failed to Create Categery'
+        );
+    }
+};
+
+export async function GET() {
+    try {
+        const [selectQuery] = await db.query(
+            getCategories(),
+            {
+                userID: await fetchUserID()
+            }
+        );
+
+        return success({
+            data: selectQuery
+        });
+    } catch (error) {
+        return fail(
+            500,
+            error instanceof Error
+                ? error.message
+                : 'Failed to Fetch Categories'
+        );
+    }
+};
