8b6bffb4cb9f79688866023ccc0eca1ebc231cca
Justin Miguel Reyes <virozzegt@gmail.com>
2025-11-02 18:57:26 +0800

    feat: (#7) create category with icon

--- app/pages/categories/add/page.tsx
+++ app/pages/categories/add/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useState, useEffect } from "react";
+import { useState, useEffect, createElement } from "react";
 import { useRouter } from "next/navigation";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
@@ -20,12 +20,24 @@ import {
   FormLabel,
   FormMessage,
 } from "@/components/ui/form";
+import {
+  Drawer,
+  DrawerClose,
+  DrawerContent,
+  DrawerDescription,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+} from "@/components/ui/drawer";
 import z from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { createCategory } from "@/store/categories.store";
 import { createCategorySchema } from "@/schema/categories.schema";
 import { toast } from "sonner";
+import { Home, Utensils, Car, ShoppingBag, Film, Heart, BookOpen, DollarSign, Gift, Plane, Zap, HomeIcon, Shield, CreditCard, TrendingUp, Package, SquareDashed } from 'lucide-react';
+import { Card } from "@/components/ui/card";
 
 export default function CreateCategory() {
     const [isLoading, setIsLoading] = useState(false);
@@ -45,7 +57,8 @@ export default function CreateCategory() {
         resolver: zodResolver(createCategorySchema),
         defaultValues: {
             name: "",
-            type: ""
+            type: "",
+						icon: ""
         }
     });
 
@@ -58,12 +71,31 @@ export default function CreateCategory() {
                 router.push('/pages/transactions');
             })
             .catch((error) => {
-                toast.error(error);
+                toast.error(error.message);
             })
             .finally(() => {
                 setIsLoading(false);
             })
     };
+
+		const icons = {
+			home: Home,
+			food: Utensils,
+			car: Car,
+			shopping: ShoppingBag,
+			entertainment: Film,
+			health: Heart,
+			education: BookOpen,
+			salary: DollarSign,
+			gift: Gift,
+			travel: Plane,
+			utilities: Zap,
+			rent: HomeIcon,
+			insurance: Shield,
+			loan: CreditCard,
+			investment: TrendingUp,
+			other: Package,
+		};
 
     return (
 		<main className='flex flex-col space-y-4 p-3'>
@@ -72,6 +104,60 @@ export default function CreateCategory() {
 			</TypographyH3>
 			<Form {...form}>
 				<form onSubmit={form.handleSubmit(onSubmit)} className='flex flex-col space-y-4'>
+					<FormField
+						control={form.control}
+						name="icon"
+						render={({ field }) => (
+								<FormItem className="m-auto mb-4 flex flex-col justify-center items-center">
+									<FormLabel className="text-md font-medium">
+										Category Icon
+									</FormLabel>
+									<FormControl>
+										<div className="flex items-center gap-2">
+											<Drawer>
+												<DrawerTrigger asChild>
+													<div className="h-28 w-28 rounded-xl border-2 border-black bg-white cursor-pointer flex items-center justify-center">
+														{field.value ? (
+															createElement(icons[field.value as keyof typeof icons], { size: 60 })
+														) : (
+															<SquareDashed size={60} className="text-gray-400"/>
+														)}
+													</div>
+												</DrawerTrigger>
+												<DrawerContent className="max-h-[80vh] p-4">
+														<DrawerHeader>
+																<DrawerTitle>Select an Icon</DrawerTitle>
+														</DrawerHeader>
+														<div className="w-full grid grid-cols-4 gap-2 mb-4"> 
+																{Object.keys(icons).map((iconName) => (
+																		<div
+																				key={iconName}
+																				className="flex items-center justify-center"
+																				onClick={() => {
+																						field.onChange(iconName);
+																				}}
+																		>
+																				<Card className="bg-primary border-2 w-full flex justify-center items-center text-white">
+																						{createElement(icons[iconName as keyof typeof icons], {size: 32})}
+																				</Card>
+																		</div>
+																))}
+														</div>
+														<DrawerFooter>
+																<DrawerClose asChild>
+																		<Button>
+																				Done
+																		</Button>
+																</DrawerClose>
+														</DrawerFooter>
+												</DrawerContent>
+												</Drawer>
+										</div>
+									</FormControl>
+									<FormMessage className="text-center"/>
+								</FormItem>
+						)}
+					/>
 				 	<FormField
 						control={form.control}
 						name="name"
