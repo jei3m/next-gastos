e1345a2748877fee680695a4ac3780d3f1dc8847
Justin Miguel Reyes <virozzegt@gmail.com>
2025-11-08 16:29:39 +0800

    feat: (#7) edit category schema

--- schema/categories.schema.ts
+++ schema/categories.schema.ts
@@ -11,3 +11,15 @@ export const createCategorySchema = z.object({
         message: 'Please select a category icon',
     })
 });
+
+export const editCategorySchema = z.object({
+    name: z.string().max(15, {
+        message: 'Name has a maximum of 15 characters only',
+    }),
+    type: z.string().min(1, {
+        message: 'Please select a category type',
+    }),
+    icon: z.string().min(1, {
+        message: 'Please select a category icon',
+    })
+});
