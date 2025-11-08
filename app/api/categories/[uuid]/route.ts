472dfc6690a5ca71940212f8d316be4d58fe2219
Justin Miguel Reyes <virozzegt@gmail.com>
2025-11-08 16:26:51 +0800

    ref: tab spacing, code format

--- app/api/categories/[uuid]/route.ts
+++ app/api/categories/[uuid]/route.ts
@@ -3,98 +3,130 @@ import { connection, db } from "@/utils/db";
 import { success, fail } from "@/utils/helpers";
 import { responseRow } from "@/types/response.types";
 import { fetchUserID } from '@/lib/auth-session';
-import { deleteCategory, updateCategory } from "@/sql/categories/categories.sql";
+import { 
+	getCategoryByID, 
+	deleteCategory, 
+	updateCategory 
+} from "@/sql/categories/categories.sql";
+
+export async function GET(
+	_req: Request,
+	{ params }: { params: Promise<{ uuid: string }> }
+) {
+	try {
+		const { uuid } = await params;
+
+		const [rows] = await db.query(
+			getCategoryByID(),
+			{
+				userID: await fetchUserID(),
+				uuid
+			}
+		);
+
+		return success({ data: rows });
+	} catch (error) {
+		return fail(
+			500,
+			error instanceof Error
+				? error.message
+				: 'Failed to Fetch Category'
+		);
+	} finally {
+		connection.release();
+	}
+}
 
 export async function PUT(
-    req: NextRequest,
-    { params }: { params: Promise<{uuid: string}> }
+	req: NextRequest,
+	{ params }: { params: Promise<{ uuid: string }> }
 ) {
-    try {
-        const {
-            name,
-            type,
-            icon
-        } = await req.json();
-        const { uuid } = await params;
-        
-        const [resultUpdate] = await db.query<responseRow[]>(
-            updateCategory(),
-            {   
-                actionType: 'update',
-                uuid,
-                userID: await fetchUserID(),
-                name,
-                type,
-                icon
-            }
-        );
+	try {
+		const {
+			name,
+			type,
+			icon
+		} = await req.json();
+		const { uuid } = await params;
 
-        const parsedData = JSON.parse(
-            resultUpdate[1][0].response
-        );
+		const [resultUpdate] = await db.query<responseRow[]>(
+			updateCategory(),
+			{
+				actionType: 'update',
+				uuid,
+				userID: await fetchUserID(),
+				name,
+				type,
+				icon
+			}
+		);
 
-        if (parsedData.responseCode !== 200) {
-            return fail(
-                parsedData.responseCode,
-                parsedData.responseMessage
-            )
-        };
+		const parsedData = JSON.parse(
+			resultUpdate[1][0].response
+		);
 
-        return success({
-            data: parsedData
-        });
+		if (parsedData.responseCode !== 200) {
+			return fail(
+				parsedData.responseCode,
+				parsedData.responseMessage
+			)
+		};
 
-    } catch (error) {
-        return fail(
-            500,
-            error instanceof Error
-                ? error.message
-                : 'Failed to Update Category'
-        );
-    } finally {
-        connection.release();
-    }
+		return success({
+			data: parsedData
+		});
+
+	} catch (error) {
+		return fail(
+			500,
+			error instanceof Error
+				? error.message
+				: 'Failed to Update Category'
+		);
+	} finally {
+		connection.release();
+	}
 };
 
 export async function DELETE(
-    _req: NextRequest,
-    { params }: {params: Promise<{ uuid: string }> }
+	_req: NextRequest,
+	{ params }: { params: Promise<{ uuid: string }> }
 ) {
-    try {
-        const { uuid } = await params;
+	try {
+		const { uuid } = await params;
 
-        const [resultDelete] = await db.query<responseRow[]>(
-            deleteCategory(),
-            {
-                actionType: 'delete',
-                uuid,
-                userID: await fetchUserID()
-            }
-        );
+		const [resultDelete] = await db.query<responseRow[]>(
+			deleteCategory(),
+			{
+				actionType: 'delete',
+				uuid,
+				userID: await fetchUserID()
+			}
+		);
 
-        const parsedData = JSON.parse(
-            resultDelete[1][0].response
-        );
+		const parsedData = JSON.parse(
+			resultDelete[1][0].response
+		);
 
-        if (parsedData.responseMessage !== 200) {
-            return fail(
-                parsedData.responseCode,
-                parsedData.responseMessage
-            )
-        };
+		if (parsedData.responseMessage !== 200) {
+			return fail(
+				parsedData.responseCode,
+				parsedData.responseMessage
+			)
+		};
 
-        return success({
-            data: parsedData
-        });
+		return success({
+			data: parsedData
+		});
 
-    } catch (error) {
-        return fail(
-            500,
-            error instanceof Error
-                ? error.message
-                : 'Failed to Delete Category'
-        )
-    } finally {
-        connection.release();
-    }
+	} catch (error) {
+		return fail(
+			500,
+			error instanceof Error
+				? error.message
+				: 'Failed to Delete Category'
+		)
+	} finally {
+		connection.release();
+	}
 };
