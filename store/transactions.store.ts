b2f6ea1e738356656351027c2b2049057e76e3e5
Justin Miguel Reyes <virozzegt@gmail.com>
2025-11-16 19:07:39 +0800

    feat: (#6) store functions for get and get specific transactions

--- store/transactions.store.ts
+++ store/transactions.store.ts
@@ -2,7 +2,6 @@ import {
   CreateTransaction
 } from "@/types/transactions.types";
 
-
 export const createTransaction = async(transaction: CreateTransaction) => {
   try {
     const res = await fetch('/api/transactions', {
@@ -18,5 +17,37 @@ export const createTransaction = async(transaction: CreateTransaction) => {
       throw Error(error.message);
     };
     throw Error('Failed to Create Transaction');
+  }
+};
+
+export const fetchTransactions = async(
+  selectedAccountID: string, 
+  dateStart: string, 
+  dateEnd: string
+) => {
+  try {
+    const res = await fetch(`/api/transactions?accountID=${selectedAccountID}&dateStart=${dateStart}&dateEnd=${dateEnd}`)
+    const data = await res.json();
+    return(data.data);
+  } catch (error) {
+    if (error instanceof Error) {
+      throw Error(error.message)
+    };
+    throw Error("Failed to Fetch Transactions")
+  }
+};
+
+export const fetchTransactionByID = async(
+  uuid: string, 
+) => {
+  try {
+    const res = await fetch(`/api/transactions/${uuid}`)
+    const data = await res.json();
+    return(data.data);
+  } catch (error) {
+    if (error instanceof Error) {
+      throw Error(error.message)
+    };
+    throw Error("Failed to Fetch Transaction")
   }
 };
