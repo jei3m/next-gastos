81a28184b24f658d888ae7142c90e258cec8625e
Justin Miguel Reyes <virozzegt@gmail.com>
2025-11-21 21:25:19 +0800

    del: transfer option

--- lib/data.ts
+++ lib/data.ts
@@ -49,4 +49,4 @@ export const transactions = [
 ];
 export const categoryTypes = ["Expense", "Income"];
 
-export const transactionTypes = ["Expense", "Income", "Transfer"];
+export const transactionTypes = ["Expense", "Income"];
