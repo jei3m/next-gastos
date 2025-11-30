f3906229c5c8a53a64c2f2b2b834704ab816b8a5
Justin Miguel Reyes <virozzegt@gmail.com>
2025-11-30 17:31:29 +0800

    style: max-width

--- app/auth/layout.tsx
+++ app/auth/layout.tsx
@@ -2,8 +2,8 @@ export default function AuthLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
-    <>
+    <div className="h-full max-w-[600px] m-auto">
       {children}
-    </>
+    </div>
   );
 };
