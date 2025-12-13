5aaf713117c878065cbcef7160cfea69e198e484
Justin Miguel Reyes <virozzegt@gmail.com>
2025-12-13 11:42:52 +0800

    fix: wrap in suspense

--- app/pages/layout.tsx
+++ app/pages/layout.tsx
@@ -1,21 +1,25 @@
 import Navbar from "@/components/custom/navbar";
 import { Dock } from "@/components/custom/dock";
 import { Toaster } from "@/components/ui/sonner";
+import { Suspense } from "react";
+import Loader from "@/components/custom/loader";
 
 export default function TransactionsLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
-    <div className="max-w-[600px] m-auto">
-      <Navbar/>
-      {children}
-      <Toaster
-        position="top-center"
-        expand={false}
-      />
-      <Dock
-        showLabels={true}
-      />
-    </div>
+    <Suspense fallback={<Loader/>}>
+      <div className="max-w-[600px] m-auto">
+        <Navbar/>
+        {children}
+        <Toaster
+          position="top-center"
+          expand={false}
+        />
+        <Dock
+          showLabels={true}
+        />
+      </div>      
+    </Suspense>
   );
 };
