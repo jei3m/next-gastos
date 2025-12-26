8cfdc02d90ede31c4b046078f49173793cc6775e
Justin Miguel Reyes <virozzegt@gmail.com>
2025-12-26 11:48:23 +0800

    feat: dynamic display of sidebar or dock

--- app/pages/layout.tsx
+++ app/pages/layout.tsx
@@ -3,23 +3,30 @@ import { Dock } from "@/components/custom/dock";
 import { Toaster } from "@/components/ui/sonner";
 import { Suspense } from "react";
 import Loader from "@/components/custom/loader";
+import { SidebarProvider } from "@/components/ui/sidebar";
+import { AppSidebar } from "@/components/ui/app-sidebar";
 
 export default function TransactionsLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
     <Suspense fallback={<Loader/>}>
-      <div className="max-w-[700px] m-auto">
-        <Navbar/>
-        {children}
-        <Toaster
-          position="top-center"
-          expand={false}
-        />
-        <Dock
-          showLabels={true}
-        />
-      </div>
+      <SidebarProvider>
+        <AppSidebar />
+        <div className="w-full">
+          <Navbar/>
+          <div className="max-w-[800px] m-auto">
+            {children}
+          </div>
+          <Toaster
+            position="top-center"
+            expand={false}
+          />
+          <Dock
+            showLabels={true}
+          />   
+        </div>
+      </SidebarProvider>
     </Suspense>
   );
 };
