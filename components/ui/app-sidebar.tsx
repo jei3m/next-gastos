235b93093dba5636feaa12b3ea6cc4b9a219d587
Justin Miguel Reyes <virozzegt@gmail.com>
2025-12-27 14:58:18 +0800

    style: bg color depending on pathName

--- components/ui/app-sidebar.tsx
+++ components/ui/app-sidebar.tsx
@@ -14,9 +14,12 @@ import AccountSelector from "../custom/account-selector";
 import { Separator } from "./separator";
 import { useIsMobile } from "@/hooks/use-mobile";
 import Link from "next/link";
+import { usePathname } from 'next/navigation';
+import { cn } from "@/lib/utils";
 
 export function AppSidebar() {
   const isMobile = useIsMobile();
+  const pathName = usePathname();
   return (
     <>
       {!isMobile && (
@@ -37,22 +40,28 @@ export function AppSidebar() {
               </SidebarGroupLabel>
               <SidebarGroupContent>
                 <SidebarMenu>
-                  {menuItems.map((item) => (
-                    <SidebarMenuItem key={item.label} className="mb-1">
-                      <SidebarMenuButton 
-                        asChild 
-                        className="px-2 py-2 hover:bg-muted/50 transition-colors rounded-md"
-                      >
-                        <Link
-                          href={item.route} 
-                          className="flex items-center gap-2 text-sm text-foreground hover:text-foreground"
+                  {menuItems.map((item) => {
+                    const isActive = pathName === item.route;
+                    return(
+                      <SidebarMenuItem key={item.label} className="mb-1">
+                        <SidebarMenuButton 
+                          asChild 
+                          className={cn(
+                            'px-2 py-2 hover:bg-muted-foreground/20 transition-colors rounded-md',
+                            isActive && 'bg-muted-foreground/20'
+                          )}
                         >
-                          <item.icon className="h-4 w-4" />
-                          <span>{item.label[0].toUpperCase() + item.label.slice(1)}</span>
-                        </Link>
-                      </SidebarMenuButton>
-                    </SidebarMenuItem>
-                  ))}
+                          <Link
+                            href={item.route} 
+                            className='flex items-center gap-2 text-sm text-foreground hover:text-foreground'
+                          >
+                            <item.icon className="h-4 w-4" />
+                            <span>{item.label[0].toUpperCase() + item.label.slice(1)}</span>
+                          </Link>
+                        </SidebarMenuButton>
+                      </SidebarMenuItem>                      
+                    )
+                  })}
                 </SidebarMenu>
               </SidebarGroupContent>
             </SidebarGroup>
