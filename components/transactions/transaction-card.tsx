ee9086456ab3f41afab0780a2907581809b2f938
Justin Miguel Reyes <virozzegt@gmail.com>
2025-12-31 10:49:27 +0800

    feat: popover for totalIncome and totalExpense

--- components/transactions/transaction-card.tsx
+++ components/transactions/transaction-card.tsx
@@ -11,6 +11,12 @@ import {
   TransactionDetails 
 } from "@/types/transactions.types";
 import { formatAmount } from "@/utils/format-amount";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover"
+import { InfoIcon } from "lucide-react";
 
 interface TransactionCardProps {
   transaction: Transaction
@@ -22,24 +28,62 @@ function TransactionCard({
   return (
     <Card className='border-2'>
       <CardHeader>
-        <CardTitle className='flex justify-between'>
+        <CardTitle className='flex justify-between items-center'>
           <span>
             {new Date(transaction.date).toLocaleDateString('en-US', {
               month: 'long',
               day: 'numeric'
             })}
           </span>
-          <span
-            className={
-              `${
-                transaction.total.startsWith('-')
-                  ? 'text-red-500'
-                  : 'text-primary'
-              }`
-            }
-          >
-            PHP {formatAmount(transaction.total)}
-          </span>
+          <div className="flex items-center gap-2">
+            <Popover>
+              <PopoverTrigger>
+                <InfoIcon className="text-muted-foreground -mt-[1px]" size={20}/>
+              </PopoverTrigger>
+              <PopoverContent className="w-auto flex flex-col p-2 text-sm md:text-md">
+                <div className="flex items-center gap-2">
+                  <span className="font-semibold">Income:</span>
+                  <span
+                    className='text-primary'
+                  >
+                    {
+                      Number.parseFloat(transaction.totalIncome) > 0
+                        ? ' +'
+                        : ' '
+                    }
+                    {formatAmount(transaction.totalIncome)}
+                  </span>
+                </div>
+                <div className="flex items-center gap-2">
+                  <span className="font-semibold">Expense:</span>
+                  <span
+                    className='text-red-500'
+                  >
+                    {formatAmount(transaction.totalExpense)}
+                  </span>
+                </div>
+              </PopoverContent>
+            </Popover>
+            <span
+              className={
+                `${
+                  transaction.total.startsWith('-')
+                    ? 'text-red-500'
+                    : 'text-primary'
+                }`
+              }
+            >
+              PHP 
+              {
+                transaction.total.startsWith('-') 
+                  ? ' '
+                  : Number.parseFloat(transaction.total) > 0
+                    ? ' +'
+                    : ' '
+              }
+              {formatAmount(transaction.total)}
+            </span>
+          </div>
         </CardTitle>
       </CardHeader>
       <div className='w-full border-t border-gray-300' />
