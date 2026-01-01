dc1059a0da1a5c94f5e2ed8b36d075315714649b
Justin Miguel Reyes <virozzegt@gmail.com>
2026-01-01 11:40:11 +0800

    feat: custom alert dialog component

---
+++ components/custom/custom-alert-dialog.tsx
@@ -0,0 +1,65 @@
+import { 
+  MouseEventHandler, 
+  ReactNode 
+} from "react";
+import {
+  AlertDialog,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "../ui/alert-dialog";
+import { Button } from "../ui/button";
+
+interface AlertDialogProps {
+  isDisabled: boolean;
+  trigger: ReactNode;
+  title: string;
+  description: ReactNode;
+  confirmMessage: string;
+  onConfirm: MouseEventHandler<HTMLButtonElement>;
+};
+
+export default function CustomAlertDialog({
+  isDisabled,
+  trigger,
+  title,
+  description,
+  confirmMessage,
+  onConfirm
+}: AlertDialogProps) {
+  return (
+    <AlertDialog>
+      <AlertDialogTrigger
+        className="text-red-500"
+        disabled={isDisabled}
+      >
+        {trigger}
+      </AlertDialogTrigger>
+      <AlertDialogContent
+        className="border-2 bg-primary [&>button]:hidden"
+        onOpenAutoFocus={(e) => e.preventDefault()}
+      >
+        <AlertDialogHeader className="text-left">
+          <AlertDialogTitle>{title}</AlertDialogTitle>
+          <AlertDialogDescription className="text-gray-800">
+            {description}
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter className="flex flex-row justify-between">
+          <AlertDialogCancel asChild>
+            <Button variant="outline" className="border-2">
+              Cancel
+            </Button>
+          </AlertDialogCancel>
+          <Button variant="destructive" className="border-2" onClick={onConfirm}>
+            {confirmMessage}
+          </Button>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  );
+};
