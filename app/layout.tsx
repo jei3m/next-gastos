3d590bbd1f0354d7efc420ecf99b686a0d1d294d
Justin Miguel Reyes <virozzegt@gmail.com>
2026-01-17 17:25:01 +0800

    ref: code formatting of root page, layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,29 @@
-import type { Metadata, Viewport } from "next";
-import { Poppins } from "next/font/google"; // Already imported
-import "./globals.css";
-import { AccountProvider } from "@/context/account-context";
-import { TanstackProvider } from "@/context/tanstack-context";
+import type { Metadata, Viewport } from 'next';
+import { Poppins } from 'next/font/google'; // Already imported
+import './globals.css';
+import { AccountProvider } from '@/context/account-context';
+import { TanstackProvider } from '@/context/tanstack-context';
 
-const APP_NAME = "Gastos";
-const APP_DEFAULT_TITLE = "Gastos";
-const APP_TITLE_TEMPLATE = "%s - Gastos";
-const APP_DESCRIPTION = "Finance tracker PWA built with Next JS";
+const APP_NAME = 'Gastos';
+const APP_DEFAULT_TITLE = 'Gastos';
+const APP_TITLE_TEMPLATE = '%s - Gastos';
+const APP_DESCRIPTION =
+  'Finance tracker PWA built with Next JS';
 
 const poppins = Poppins({
-  subsets: ["latin"],
-  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
-  variable: "--font-poppins",
+  subsets: ['latin'],
+  weight: [
+    '100',
+    '200',
+    '300',
+    '400',
+    '500',
+    '600',
+    '700',
+    '800',
+    '900',
+  ],
+  variable: '--font-poppins',
 });
 
 export const metadata: Metadata = {
@@ -22,20 +33,20 @@ export const metadata: Metadata = {
     template: APP_TITLE_TEMPLATE,
   },
   description: APP_DESCRIPTION,
-  manifest: "/manifest.json",
+  manifest: '/manifest.json',
   appleWebApp: {
     capable: true,
     title: APP_DEFAULT_TITLE,
-    statusBarStyle: "default",
+    statusBarStyle: 'default',
   },
   icons: {
-    apple: "/icons/apple-touch-icon.png",
+    apple: '/icons/apple-touch-icon.png',
   },
   formatDetection: {
     telephone: false,
   },
   openGraph: {
-    type: "website",
+    type: 'website',
     siteName: APP_NAME,
     title: {
       default: APP_DEFAULT_TITLE,
@@ -44,7 +55,7 @@ export const metadata: Metadata = {
     description: APP_DESCRIPTION,
   },
   twitter: {
-    card: "summary",
+    card: 'summary',
     title: {
       default: APP_DEFAULT_TITLE,
       template: APP_TITLE_TEMPLATE,
@@ -54,21 +65,21 @@ export const metadata: Metadata = {
 };
 
 export const viewport: Viewport = {
-  themeColor: "#FFFFFF",
+  themeColor: '#FFFFFF',
 };
 
 export default function RootLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
-      <html lang="en">
-        <body className={`${poppins.variable} font-sans antialiased m-auto max-w-[600px]`}>
-          <TanstackProvider>
-            <AccountProvider>
-              {children}
-            </AccountProvider>            
-          </TanstackProvider>
-        </body>
-      </html>
+    <html lang="en">
+      <body
+        className={`${poppins.variable} font-sans antialiased m-auto max-w-[600px]`}
+      >
+        <TanstackProvider>
+          <AccountProvider>{children}</AccountProvider>
+        </TanstackProvider>
+      </body>
+    </html>
   );
 }
