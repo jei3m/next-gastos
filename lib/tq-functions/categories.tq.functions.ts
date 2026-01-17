99d2030045815f0806adad72be606e41e8ab0938
Justin Miguel Reyes <virozzegt@gmail.com>
2026-01-17 17:18:21 +0800

    ref: code formatting, remove sorting (rely on be)

--- lib/tq-functions/categories.tq.functions.ts
+++ lib/tq-functions/categories.tq.functions.ts
@@ -1,51 +1,40 @@
 import {
-	CategoryData,
-	CreateCategory,
-	EditCategory
-} from "@/types/categories.types";
+  CreateCategory,
+  EditCategory,
+} from '@/types/categories.types';
 
 export const fetchCategories = async (
   type: string | null,
   accountID: string | null,
   dateStart?: string | null,
   dateEnd?: string | null,
-	filter?: string
+  filter?: string
 ) => {
   try {
     const params = new URLSearchParams();
-    
+
     if (type) params.append('type', type);
     if (accountID) params.append('accountID', accountID);
     if (dateStart) params.append('dateStart', dateStart);
     if (dateEnd) params.append('dateEnd', dateEnd);
     if (filter) params.append('filter', filter);
-    
-    const res = await fetch(`/api/categories?${params.toString() || ''}`, {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    });
+
+    const res = await fetch(
+      `/api/categories?${params.toString() || ''}`,
+      {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      }
+    );
     const data = await res.json();
-    
+
     if (!data.success) {
       throw new Error(data.message);
-    };
+    }
 
-    if (filter === 'list') {
-      return data.data
-    } else {
-			const sortedData = JSON.parse(JSON.stringify(data.data));
-			sortedData.forEach((category: CategoryData) => {
-				if (category.details) {
-					category.details.sort((a, b) => {
-						return a.name.localeCompare(b.name);
-					})
-				}
-			});
-    	return sortedData;
-		};
-
+    return data.data;
   } catch (error) {
     console.error('fetchCategories error:', error);
     if (error instanceof Error) {
@@ -56,78 +45,83 @@ export const fetchCategories = async (
 };
 
 export const fetchCategoryByID = async (id: string) => {
-	try {
-		const res = await fetch(`/api/categories/${id}`, {
-			method: 'GET',
-		});
-		const data = await res.json();
+  try {
+    const res = await fetch(`/api/categories/${id}`, {
+      method: 'GET',
+    });
+    const data = await res.json();
     if (!data.success) {
-      throw Error(data.message)
-    };
-		return (data.data[0] || null)
-	} catch (error) {
-		if (error instanceof Error) {
-			throw Error(error.message)
-		};
-		throw Error('Failed to Fetch Category');
-	}
+      throw Error(data.message);
+    }
+    return data.data[0] || null;
+  } catch (error) {
+    if (error instanceof Error) {
+      throw Error(error.message);
+    }
+    throw Error('Failed to Fetch Category');
+  }
 };
 
-export const createCategory = async (category: CreateCategory) => {
-	try {
-		const res = await fetch('/api/categories', {
-			method: 'POST',
-			headers: { 'Content-Type': 'application/json' },
-			body: JSON.stringify(category)
-		});
-		const data = await res.json();
+export const createCategory = async (
+  category: CreateCategory
+) => {
+  try {
+    const res = await fetch('/api/categories', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(category),
+    });
+    const data = await res.json();
     if (!data.success) {
-      throw Error(data.message)
-    };
-		return (data.data);
-	} catch (error) {
-		if (error instanceof Error) {
-			throw Error(error.message);
-		};
-		throw Error('Failed to Create Category');
-	};
+      throw Error(data.message);
+    }
+    return data.data;
+  } catch (error) {
+    if (error instanceof Error) {
+      throw Error(error.message);
+    }
+    throw Error('Failed to Create Category');
+  }
 };
 
-export const editCategory = async (id: string, category: EditCategory) => {
-	try {
-		const res = await fetch(`/api/categories/${id}`, {
-			method: 'PUT',
-			headers: { 'Content-Type': 'application/json' },
-			body: JSON.stringify(category)
-		});
-		const data = await res.json();
+export const editCategory = async (
+  id: string,
+  category: EditCategory
+) => {
+  try {
+    const res = await fetch(`/api/categories/${id}`, {
+      method: 'PUT',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(category),
+    });
+    const data = await res.json();
     if (!data.success) {
-      throw Error(data.message)
-    };
-		return (data.data);
-	} catch (error) {
-		if (error instanceof Error) {
-			throw Error(error.message);
-		};
-		throw Error('Failed to Edit Category');
-	};
+      throw Error(data.message);
+    }
+    return data.data;
+  } catch (error) {
+    if (error instanceof Error) {
+      throw Error(error.message);
+    }
+    throw Error('Failed to Edit Category');
+  }
 };
 
 export const deleteCategory = async (id: string) => {
-	try {
-		const res = await fetch(`/api/categories/${id}`, {
-			method: 'DELETE',
-			headers: { 'Content-Type': 'application/json' },
-		});
-		const data = await res.json();
+  try {
+    const res = await fetch(`/api/categories/${id}`, {
+      method: 'DELETE',
+      headers: { 'Content-Type': 'application/json' },
+    });
+    const data = await res.json();
     if (!data.success) {
-      throw Error(data.message)
-    };
-		return (data.data);
-	} catch (error) {
-		if (error instanceof Error) {
-			throw Error(error.message);
-		};
-		throw Error('Failed to Delete Category');
-	};
+      throw Error(data.message);
+    }
+    return data.data;
+  } catch (error) {
+    if (error instanceof Error) {
+      throw Error(error.message);
+    }
+    throw Error('Failed to Delete Category');
+  }
 };
