fe0667e9921b2adfb8f074ffe253c36a15cd0680
Justin Miguel Reyes <virozzegt@gmail.com>
2026-01-17 17:22:58 +0800

    chore: code formatting

--- proxy.ts
+++ proxy.ts
@@ -1,32 +1,34 @@
-import { NextRequest, NextResponse } from "next/server";
-import { headers } from "next/headers";
-import { auth } from "@/lib/auth/auth";
- 
+import { NextRequest, NextResponse } from 'next/server';
+import { headers } from 'next/headers';
+import { auth } from '@/lib/auth/auth';
+
 export async function proxy(request: NextRequest) {
-    const session = await auth.api.getSession({
-        headers: await headers()
-    })
- 
-    if (!session) {
-        // Return JSON response for API routes
-        if (request.nextUrl.pathname.startsWith('/api/')) {
-            return NextResponse.json(
-                {error: "Unauthorized"},
-                {status: 401},
-            );
-        }      
-        // Page redirect for non API routes
-        return NextResponse.redirect(new URL("/auth/login", request.url));
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+
+  if (!session) {
+    // Return JSON response for API routes
+    if (request.nextUrl.pathname.startsWith('/api/')) {
+      return NextResponse.json(
+        { error: 'Unauthorized' },
+        { status: 401 }
+      );
     }
- 
-    return NextResponse.next();
+    // Page redirect for non API routes
+    return NextResponse.redirect(
+      new URL('/auth/login', request.url)
+    );
+  }
+
+  return NextResponse.next();
 }
- 
+
 export const config = {
-    matcher: [
-        "/pages/transactions/:path*",
-        "/pages/categories/:path*",
-        "/pages/accounts/:path*",
-        "/api/((?!auth).*)"
-	]
+  matcher: [
+    '/pages/transactions/:path*',
+    '/pages/categories/:path*',
+    '/pages/accounts/:path*',
+    '/api/((?!auth).*)',
+  ],
 };
