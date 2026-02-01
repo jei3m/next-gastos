b18002b102c4b4981b45a4b5a8e103831084c2f4
Justin Miguel Reyes <virozzegt@gmail.com>
2026-02-01 14:54:47 +0800

    feat: get transactions and transaction count by category sql query

--- lib/sql/transactions/transactions.sql.ts
+++ lib/sql/transactions/transactions.sql.ts
@@ -63,6 +63,92 @@ export const getTransactionsCount = () => {
                 AND accountID = :accountID;`;
 };
 
+export const getTransactionsByCategory = () => {
+  return `
+        WITH transactions_cte AS (
+            SELECT
+                t.id,
+                t.date,
+                (t.amount + t.transfer_fee) AS amount,
+                c.name,
+                t.note,
+                t.type,
+                t.time,
+                t.ref_user_id,
+                t.ref_accounts_id,
+                ROW_NUMBER() OVER (PARTITION BY t.date ORDER BY t.time ASC) as time_order
+            FROM v_transactions_table t
+            LEFT JOIN v_categories_table c on t.ref_categories_id = c.id
+            WHERE t.ref_categories_id = :categoryID
+                AND t.ref_user_id = :userID
+                AND t.ref_accounts_id = :accountID
+                AND (:dateStart IS NULL OR t.date >= :dateStart)
+                AND (:dateEnd IS NULL OR t.date <= :dateEnd)
+            GROUP BY
+                t.id,
+                t.date,
+                t.amount,
+                c.name,
+                t.note,
+                t.type,
+                t.time,
+                t.ref_user_id,
+                t.ref_accounts_id
+            ORDER BY
+                date DESC,
+                time DESC
+        )
+        SELECT
+            date,
+            CONCAT('+', SUM(CASE WHEN type = 'income' THEN amount ELSE 0 END)) AS totalIncome,
+            CASE
+                WHEN SUM(CASE WHEN type = 'expense' THEN amount ELSE 0 END) > 0
+                    THEN CONCAT('-', SUM(CASE WHEN type = 'expense' THEN amount ELSE 0 END))
+                ELSE
+                    SUM(CASE WHEN type = 'expense' THEN amount ELSE 0 END)
+            END AS totalExpense,
+            CASE
+                WHEN SUM(CASE WHEN type = 'income' THEN amount ELSE -amount END) > 0
+                    THEN CONCAT('+', SUM(CASE WHEN type = 'income' THEN amount ELSE -amount END))
+                WHEN SUM(CASE WHEN type = 'income' THEN amount ELSE -amount END) = 0
+                    THEN SUM(CASE WHEN type = 'income' THEN amount ELSE -amount END)
+                ELSE
+                    CAST(SUM(CASE WHEN type = 'income' THEN amount ELSE -amount END) AS CHAR)
+            END AS total,
+            JSON_ARRAYAGG(
+                JSON_OBJECT(
+                    'id', id,
+                    'category', name,
+                    'note', note,
+                    'amount', amount,
+                    'type', type,
+                    'time', time
+                )
+            ) AS details,
+            ref_user_id AS userID,
+            ref_accounts_id AS accountID
+        FROM transactions_cte
+        GROUP BY
+            date,
+            ref_user_id,
+            ref_accounts_id
+        ORDER BY date DESC;
+    `;
+};
+
+export const getTransactionsByCategoryCount = () => {
+  return `
+        SELECT DISTINCT
+            COUNT (date) AS count
+        FROM v_transactions_table
+        WHERE ref_user_id = :userID
+            AND ref_accounts_id = :accountID
+            AND ref_categories_id = :categoryID
+            AND (:dateStart IS NULL OR date >= :dateStart)
+            AND (:dateEnd IS NULL OR date <= :dateEnd);
+    `;
+};
+
 export const updateTransaction = () => {
   return `CALL manage_transactions
             (
